d140a5e779501af7676b7d9bd78c5f2b1d55bff1
suedson <xzsued2018@gmail.com>
2025-05-21 19:49:20 -0300

    install commit

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def ola():
-    # return '<h1> Olá, Mundo</h1>
+    # return '<h1>Olá, Mundo!</h1>'
     return render_template('index.html')
 
+@app.route('/sobre-equipe')
+def sobre_equipe():
+    return render_template('sobre.html')
 
 
 app.run()
